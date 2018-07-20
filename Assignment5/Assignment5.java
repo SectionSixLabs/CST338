e6577f1034ee174aff0d21f70c84cac308acee06
Danny <Danny@Wave>
2018-07-19 20:46:05 -0700

    Working on Phase 1

--- Assignment5/Assignment5.java
+++ Assignment5/Assignment5.java
@@ -8,13 +8,72 @@
 
 //TODO Please submit one file for each phase (3 .txt files, UML .pdf file)
 
+//TODO Phase1
+import javax.swing.*;
+import java.awt.*;
+
 public class Assignment5
 {
 
    public static void main(String[] args)
    {
-      // TODO Auto-generated method stub
+      //Phase1
+      static final int NUM_CARD_IMAGES = 57; // 52 + 4 jokers + 1 back-of-card image
+      static Icon[] icon = new ImageIcon[NUM_CARD_IMAGES];
+      
+      static void loadCardIcons()
+      {
+         // build the file names ("AC.gif", "2C.gif", "3C.gif", "TC.gif", etc.)
+         // in a SHORT loop.  For each file name, read it in and use it to
+         // instantiate each of the 57 Icons in the icon[] array.
+      }
+      
+      // turns 0 - 13 into "A", "2", "3", ... "Q", "K", "X"
+      static String turnIntIntoCardValue(int k)
+      {
+         // an idea for a helper method (do it differently if you wish)
+      }
+      
+      // turns 0 - 3 into "C", "D", "H", "S"
+      static String turnIntIntoCardSuit(int j)
+      {
+         // an idea for another helper method (do it differently if you wish)
+      }
+      
+      // a simple main to throw all the JLabels out there for the world to see
+      public static void main(String[] args)
+      {
+         int k;
+         
+         // prepare the image icon array
+         loadCardIcons();
+         
+         // establish main frame in which program will run
+         JFrame frmMyWindow = new JFrame("Card Room");
+         frmMyWindow.setSize(1150, 650);
+         frmMyWindow.setLocationRelativeTo(null);
+         frmMyWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+         // set up layout which will control placement of buttons, etc.
+         FlowLayout layout = new FlowLayout(FlowLayout.CENTER, 5, 20);   
+         frmMyWindow.setLayout(layout);
+         
+         // prepare the image label array
+         JLabel[] labels = new JLabel[NUM_CARD_IMAGES];
+         for (k = 0; k < NUM_CARD_IMAGES; k++)
+            labels[k] = new JLabel(icon[k]);
+         
+         // place your 3 controls into frame
+         for (k = 0; k < NUM_CARD_IMAGES; k++)
+            frmMyWindow.add(labels[k]);
 
+         // show everything to the user
+         frmMyWindow.setVisible(true);
+      }
+   
+      
+      //End of main
+      System.out.println("End of main.");
    }
 
 }
