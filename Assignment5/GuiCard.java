038286e6904f6d991944bda6e29c4557cc989fce
Danny <Danny@Wave>
2018-07-21 13:29:27 -0700

    finished getBackCardIcon()

--- Assignment5/GuiCard.java
+++ Assignment5/GuiCard.java
@@ -16,9 +16,9 @@
 
 import javax.swing.*;
 
-import Card.Suit;
-
 //import Card.Suit;
+//Confused on whats in default package? Is Cards class in it since we don't need to import Card
+
 
 import java.awt.*;
 import java.io.File;
@@ -57,6 +57,10 @@ public class GuiCard
                iconCards[value][suit] = new ImageIcon(file);
             }
          }
+         //Loading the face down card separately
+         String backOfCard = path+"BK.gif";
+         System.out.println(backOfCard);
+        iconBack = new ImageIcon(backOfCard);
       }
       iconsLoaded = true; //After icons loaded true, don't need to load again
       
@@ -64,55 +68,69 @@ public class GuiCard
    
    static public Icon getIcon(Card card)
    {  
-      //Keeping consistency same as loadCardIcons maybe to make this public to class later
-      String cardValues[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9",
-            "T", "J", "Q", "K", "X"};
-      String cardSuites[] = {"C", "D", "H", "S"};
-      
-      //Remember Card has a char for Value and enum for Suit
-      
-      //get card attributes value and suit to find their index in Array
-      
-      String suitString;
-      String valueString;
-      
-      switch(card.getSuit())
+      if(iconsLoaded)
       {
-      case CLUBS:
-         suitString = "C";
-         break;
-      case HEARTS:
-         suitString = "H";
-         break;
-      case DIAMONDS:
-         suitString = "D";
-         break;
-      case SPADES:
-         suitString = "S";
-         break;
-      default:
-         suitString = "Error";
-         break;
-         
-      }
-      
-      valueString = String.valueOf(card.getValue());
-      
-      //Testing
-      System.out.println(valueString);
-      System.out.println(suitString);
+         //Keeping consistency same as loadCardIcons maybe to make this public to class later
+         String cardValues[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9",
+               "T", "J", "Q", "K", "X"};
+         String cardSuites[] = {"C", "D", "H", "S"};
 
-      //Get index based of suit and value of card
-      int intValueIndex = Arrays.asList(cardValues).indexOf(valueString);
-      int intSuitIndex = Arrays.asList(cardSuites).indexOf(suitString);
-      
-      return iconCards[intValueIndex][intSuitIndex];
-      
+         //Remember Card has a char for Value and enum for Suit
+
+         //get card attributes value and suit to find their index in Array
+
+         String suitString;
+         String valueString;
+
+         switch(card.getSuit())
+         {
+         case CLUBS:
+            suitString = "C";
+            break;
+         case HEARTS:
+            suitString = "H";
+            break;
+         case DIAMONDS:
+            suitString = "D";
+            break;
+         case SPADES:
+            suitString = "S";
+            break;
+         default:
+            suitString = "Error";
+            break;
+
+         }
+
+         valueString = String.valueOf(card.getValue());
+
+         //Testing ~comment out
+         System.out.println(valueString);
+         System.out.println(suitString);
+
+         //Get index based of suit and value of card
+         int intValueIndex = Arrays.asList(cardValues).indexOf(valueString);
+         int intSuitIndex = Arrays.asList(cardSuites).indexOf(suitString);
+
+         return iconCards[intValueIndex][intSuitIndex];
+      }
+      else
+      {
+         return null;
+      }
+
    }
    
    static public Icon getBackCardIcon()
    {
-      //TODO
+      if(iconsLoaded)
+      {
+         return iconBack;
+      }
+      else
+      {
+         return null;
+      }
       
    }
    //main as testing only ~remove after
